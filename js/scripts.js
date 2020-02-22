4d0ad40125ca5373b4936c675aa370e2337b4018
David <dgkilolo23@gmail.com>
2020-02-22 22:41:31 +0300

    adds hover effect to the first image of portfolio.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,29 @@ $(document).ready(function() {
   $('#img1').click(function() {
     $('.p1-showing').show();
     $('#img1').hide();
-  })
+  });
   $('.p1-showing').click(function() {
     $('.p1-showing').hide();
     $('#img1').show();
+  });
+  $('#img2').click(function() {
+    $('.p2-showing').show();
+    $('#img2').hide();
+  });
+  $('.p2-showing').click(function() {
+    $('.p2-showing').hide();
+    $('#img2').show();
+  });
+  $('#img3').click(function() {
+    $('.p3-showing').show();
+    $('#img3').hide();
+  });
+  $('.p3-showing').click(function() {
+    $('.p3-showing').hide();
+    $('#img3').show();
+  });
+  $('#test').hover(function() {
+    $('.port1').toggle()
   });
 
 });
